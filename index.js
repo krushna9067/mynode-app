49774a0bc598fe57f4a0d3df9ce8603860407116
krushna nangare <krushnanangare0>
2025-07-31 07:58:18 +0000

    commit 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();//Respond with "hello world" for requests that hit our root 
 
 app.get('/', function (req, res) { 
 
- res.send('Node JS '); 
+ res.send('Node JS'); 
 
 });//listen to port 3000 by default 
 
